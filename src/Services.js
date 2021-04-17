610ceeb158fb02812bc26b0af0deae4a48ae1f80
harshadshaik8860 <harshadshaik8860@gmail.com>
2021-04-17 08:51:07 -0700

    mdodified 2

--- src/Services.js
+++ src/Services.js
@@ -17,25 +17,30 @@ const Services = () =>{
     return(
         <div className="container">
             <div className="row">
-                <div className="col-md-12 mb-3 from-group">
-                <div className="text-success text-center">
+                <div className=" col-md-12 text-success text-center">
                     <h2> No of products : {products.length}</h2>
                 </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6"></div>
+                    <div className="col-md-6 form-group">
+                       <div className="row">
                 {
                     products.map((xpro , index)=>{
                         return(
                             
-                                    <div className="card">
-                                        <div className="card-header text-center" key={index}>
-                                            
-                                        <h3>{xpro.name}</h3>
+                            <div className="col-md-12 form-group">
+                                    <a href=""><div className="card">
+                                        <div className="card-header text-center" key={index}>  
+                                        <h5>{xpro.name}</h5>
                                         </div>
-                                        <div className="card-body">
+                                        <div className="card-body text-center">
                                                  <img src={xpro.Photo} className="img-fluid rounded"/>
                                         </div>
                                         <div className="card-footer text-center">
                                         <p>{xpro.About}</p>
                                         </div>
+                                     </div></a>
                                      </div>
                                     
  
@@ -44,9 +49,9 @@ const Services = () =>{
                 }
                 </div>
                 </div>
-
-                
-           </div>
+                </div>
+                </div>
+               
 
     )
 

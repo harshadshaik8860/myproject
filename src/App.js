93bf4e41f36da8dba6d4227d480fa49826f77f42
harshadshaik8860 <harshadshaik8860@gmail.com>
2021-04-22 02:12:52 -0700

    updated folder

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faCheckSquare, faCoffee, faHouseUser, faUsers, faUser, faPhoneAlt, faExclamationCircle} from '@fortawesome/free-solid-svg-icons';
 import {HashRouter , Route} from 'react-router-dom';
-import Header from './header';
-import Home from './home';
-import Services from './Services'
-import Advertisement from './Advertisement';
-import ContactUs from './contactus';
-import AboutUs from './aboutus';
-import Login from './login';
+import Header from './Components/header';
+import Home from './Components/home';
+import Services from './Components/Services'
+import Advertisement from './Components/Advertisement';
+import ContactUs from './Components/contactus';
+import AboutUs from './Components/aboutus';
+import Login from './Components/login';
 //import SignUp from './signup';
-import Register from './signup';
+import Register from './Components/signup';
 
 
 
